4cab6cf0c432f26a37c0eed04a01c72dddd444b1
Synergie van der Pluijm <synergievdp@gmail.com>
2021-01-03 14:55:23 +0100

    Add Fraction class

---
+++ CalculatorApp/CalculatorApp/Fraction.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CalculatorApp {
+    public class Fraction {
+        public static Fraction ZERO => Create(0, 1);
+        public static Fraction HUNDRED => Create(100, 1);
+        private int numerator;
+        private int denominator;
+        private Fraction(int numerator, int denom) {
+            this.numerator = numerator;
+            this.denominator = denom;
+        }
+
+        public static Fraction Create(int numerator, int denominator) {
+            return new Fraction(numerator, denominator).Simplify();
+        }
+
+        public static Fraction Create(string number) {
+            string[] parts = number.Split(',');
+            int denominator = (int)Math.Pow(10, parts.Length == 1 ? 0 : parts[1].Length);
+            int numerator = (int)Decimal.Multiply(Decimal.Parse(number), denominator);
+            return Create(numerator, denominator);
+        }
+
+        public decimal ToDecimal() {
+            return Decimal.Divide(this.numerator, this.denominator);
+        }
+
+        public override string ToString() {
+            return ToDecimal().ToString("G27");
+        }
+
+        public override bool Equals(Object obj) {
+            return Equals(obj as Fraction);
+        }
+
+        public bool Equals(Fraction other) {
+            return other != null && (this.numerator == other.numerator) && (this.denominator == other.denominator);
+        }
+
+        public override int GetHashCode() {
+            return HashCode.Combine(numerator, denominator);
+        }
+
+        public Fraction Add(Fraction other) {
+            if (this.denominator != other.denominator)
+                this.CommonDenom(other);
+            this.numerator += other.numerator;
+            return this.Simplify();
+        }
+
+        public Fraction Subtract(Fraction other) {
+            if (this.denominator != other.denominator)
+                this.CommonDenom(other);
+            this.numerator -= other.numerator;
+            return this.Simplify();
+        }
+
+        public Fraction Multiply(Fraction other) {
+            this.numerator *= other.numerator;
+            this.denominator *= other.denominator;
+            return this.Simplify();
+        }
+
+        public Fraction Divide(Fraction other) {
+            this.numerator *= other.denominator;
+            this.denominator *= other.numerator;
+            return this.Simplify();
+        }
+
+        public Fraction Negate() {
+            this.numerator *= -1;
+            return this;
+        }
+
+        public Fraction CommonDenom(Fraction other) {
+            int commonDenom = this.denominator * other.denominator;
+            this.numerator *= other.denominator;
+            other.numerator *= this.denominator;
+            this.denominator = commonDenom;
+            other.denominator = commonDenom;
+            return this;
+        }
+
+        public Fraction Simplify() {
+            int factor = GCF(this.numerator, this.denominator);
+            this.numerator /= factor;
+            this.denominator /= factor;
+            return this;
+        }
+
+        public static int GCF(int numerator, int denominator) {
+            int factor = 1;
+
+            Dictionary<int, int> numPrimes = Primes(numerator);
+            Dictionary<int, int> denomPrimes = Primes(denominator);
+
+            IEnumerable<int> commonPrimes = numPrimes.Keys.Intersect(denomPrimes.Keys);
+            foreach (int prime in commonPrimes) {
+                int minCount = Math.Min(numPrimes[prime], denomPrimes[prime]);
+                factor *= (int)Math.Pow(prime, minCount);
+            }
+
+            return factor;
+        }
+
+        public static Dictionary<int, int> Primes(int number) {
+            Dictionary<int, int> primes = new Dictionary<int, int>();
+
+            double max = Math.Sqrt(number);
+            int count = 0;
+            for (int divisor = 2; divisor <= max; divisor++) {
+                count = 0;
+                if (divisor != 2 && divisor != 5 && (divisor % 2 == 0 || divisor % 5 == 0)) continue;
+                while (number % divisor == 0) {
+                    count++;
+                    number /= divisor;
+                }
+                primes.Add(divisor, count);
+            }
+
+            return primes;
+        }
+    }
+}
---
+++ CalculatorApp/CalculatorAppTests/FractionTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorApp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorApp.Tests {
+    [TestClass()]
+    public class FractionTests {
+
+        [TestMethod()]
+        public void CreateFromString() {
+            Assert.AreEqual(Fraction.Create(7, 5), Fraction.Create("1,4"));
+            Assert.AreEqual(Fraction.Create(165, 1), Fraction.Create("165"));
+        }
+
+        [TestMethod()]
+        public void SimplifyTest() {
+            Assert.AreEqual(Fraction.Create(2, 9), Fraction.Create(24, 108));
+        }
+
+        [TestMethod()]
+        public void PrimesTest() {
+            Dictionary<int, int> expected = new Dictionary<int, int>();
+            expected.Add(2, 2);
+            expected.Add(3, 1);
+            // expected.Select(i => $"{i.Key}: {i.Value}").ToList().ForEach(Console.WriteLine);
+
+            Dictionary<int, int> actual = Fraction.Primes(12);
+            // actual.Select(i => $"{i.Key}: {i.Value}").ToList().ForEach(Console.WriteLine);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GCFTest() {
+            Assert.AreEqual(12, Fraction.GCF(24, 108));
+        }
+
+        [TestMethod()]
+        public void RecurringDecimals() {
+            Assert.AreEqual(1, Fraction.Create(1, 3).Multiply(Fraction.Create(3, 1)).ToDecimal());
+        }
+
+        [TestMethod()]
+        public void BinaryRounding() {
+            Assert.AreEqual(231, Fraction.Create("1,4").Multiply(Fraction.Create("165")).ToDecimal());
+        }
+    }
+}
