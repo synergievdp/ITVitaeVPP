f3f4ac4f6331c070b7fbf27e111f49dc26829e90
Synergie van der Pluijm <synergievdp@gmail.com>
2021-01-08 16:00:55 +0100

    ...

---
+++ EncryptionApp/EncryptionApp/FileHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Text;
+
+namespace EncryptionApp {
+    public class FileHandler {
+        public void Save(string path, string input) {
+            using(StreamWriter streamWriter = File.CreateText(path)) {
+                streamWriter.Write(input);
+            }
+        }
+
+        public string Load(string path) {
+            string text = string.Empty;
+            using(StreamReader streamReader = File.OpenText(path)) {
+                text = streamReader.ReadToEnd();
+            }
+            return text;
+        }
+    }
+}
