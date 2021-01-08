36dad9313e137e896281d19eb0661d7e0d265640
Synergie van der Pluijm <synergievdp@gmail.com>
2021-01-08 09:53:41 +0100

    ...

---
+++ MorseCodeApp/MorseCodeApp/MorseCode.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace MorseCodeApp {
+    public class MorseCode {
+        Dictionary<char, string> characters;
+
+        public MorseCode() {
+            characters = new Dictionary<char, string>() {
+                {'A',"·—"},
+                {'B',"—···"},
+                {'C',"—·—·"},
+                {'D',"—··"},
+                {'E',"·"},
+                {'F',"··—·"},
+                {'G',"——·"},
+                {'H',"····"},
+                {'I',"··"},
+                {'J',"·———"},
+                {'K',"—·—"},
+                {'L',"·—··"},
+                {'M',"——"},
+                {'N',"—·"},
+                {'O',"———"},
+                {'P',"·——·"},
+                {'Q',"——·—"},
+                {'R',"·—·"},
+                {'S',"···"},
+                {'T',"—"},
+                {'U',"··—"},
+                {'V',"···—"},
+                {'W',"·——"},
+                {'X',"—··—"},
+                {'Y',"—·——"},
+                {'Z',"——··"},
+                {'0',"—————"},
+                {'1',"·————"},
+                {'2',"··———"},
+                {'3',"···——"},
+                {'4',"····—"},
+                {'5',"·····"},
+                {'6',"—····"},
+                {'7',"——···"},
+                {'8',"———··"},
+                {'9',"————·"},
+                {'.',"·—·—·—"},
+                {',',"——··——"},
+                {'?',"··——··"},
+                {'!',"—·—·——"},
+                {'-',"—····—"},
+                {'/',"—··—·"},
+                {';',"—·—·—"},
+                {':',"———···"},
+                {'\'',"·————·"},
+                {'(',"—·——·"},
+                {')',"—·——·—"},
+                {'=',"—···—"},
+                {'@',"·——·—·"},
+                {'&',"·–···"},
+            };
+        }
+
+        public string ToMorse(string text) {
+            StringBuilder output = new StringBuilder();
+            foreach(char c in text.ToUpper()) {
+                if (characters.ContainsKey(c)) {
+                    output.Append(" ");
+                    output.Append(characters[c]);
+                } else output.Append("/");
+            }
+            return output.ToString();
+        }
+
+        public string ToAlpha(string text) {
+            StringBuilder output = new StringBuilder();
+            string[] words = text.Split("/");
+            foreach(string word in words) {
+                foreach (string letter in word.Split(" ")) {
+                    foreach (char key in characters.Keys) {
+                        if (letter.Equals(characters[key])) {
+                            output.Append(key);
+                        }
+                    }
+                }
+                output.Append(" ");
+            }
+            return output.ToString();
+        }
+    }
+}
---
+++ MorseCodeApp/MorseCodeApp/ViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace MorseCodeApp {
+    public class ViewModel : Notify {
+        public MorseCode MorseCode { get; set; }
+        private string _input = "ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789 .,?!-/;\'()=@&";
+        public string Input { get { return _input; } set { _input = value; OnPropertyChanged(); } }
+        private string _output;
+        public string Output { get { return _output; } set { _output = value; OnPropertyChanged(); } }
+
+        public ViewModel() {
+            MorseCode = new MorseCode();
+            Output = MorseCode.ToMorse(Input);
+        }
+        public void OnClick(object sender, EventArgs e) {
+            switch((sender as Button).Name) {
+                case "Morse":
+                    Output = MorseCode.ToMorse(Input);
+                    break;
+                case "Alpha":
+                    Output = MorseCode.ToAlpha(Input);
+                    break;
+                case "Copy":
+                    System.Windows.Clipboard.SetText(Output);
+                    break;
+            }
+        }
+    }
+}
