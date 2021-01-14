ce96e5654c12c3e32dc35705ab37989e720c155c
Synergie van der Pluijm <synergievdp@gmail.com>
2021-01-14 16:01:02 +0100

    Initial setup.

---
+++ RomanCalculatorApp/RomanCalculatorApp/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RomanCalculatorApp {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        ViewModel ViewModel;
+
+        public MainWindow() {
+            InitializeComponent();
+
+            ViewModel = new ViewModel();
+            DataContext = ViewModel;
+        }
+    }
+
+    public class ViewModel : Notify {
+    
+    }
+
+    public class Notify : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string name = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+
+    public class RomanCalculator {
+
+    }
+}
