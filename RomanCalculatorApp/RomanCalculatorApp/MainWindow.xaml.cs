69cfc2f1a4347bcf3845d29a5207899347c9398f
Synergie van der Pluijm <synergievdp@gmail.com>
2021-01-14 18:19:15 +0100

    ...

--- RomanCalculatorApp/RomanCalculatorApp/MainWindow.xaml.cs
+++ RomanCalculatorApp/RomanCalculatorApp/MainWindow.xaml.cs
@@ -27,10 +27,69 @@ namespace RomanCalculatorApp {
             ViewModel = new ViewModel();
             DataContext = ViewModel;
         }
+
+        public void OnClick(object sender, EventArgs e) {
+            ViewModel.OnClick(sender, e);
+        }
     }
 
     public class ViewModel : Notify {
-    
+        public List<char> Buttons { get; set; } = new List<char>();
+
+        private string _input = "N";
+        public string Input { get { return _input; } set { _input = value; OnPropertyChanged(); } }
+        private string _output = "N";
+        public string Output { get { return _output; } set { _output = value; OnPropertyChanged(); } }
+        private string _operation = "N + N =";
+        public string Operation { get { return _operation; } set { _operation = value; OnPropertyChanged(); } }
+
+        public ViewModel() {
+            Buttons.AddRange(RomanNumerals.Numerals.Keys);
+            Buttons.Add('+');
+            Buttons.Add('-');
+            Buttons.Add('/');
+            Buttons.Add('x');
+        }
+
+        public void OnClick(object sender, EventArgs e) {
+            string button = (sender as Button).Content.ToString();
+            int value = 0;
+
+            switch(button) {
+                case "+":
+                    value = RomanNumerals.GetValue(Output) + RomanNumerals.GetValue(Input);
+                    Operation = String.Join(" ", Output, "+", Input, "=");
+                    Output = RomanNumerals.GetSymbol(value);
+                    Input = "N";
+                    break;
+                case "-":
+                    value = RomanNumerals.GetValue(Output) - RomanNumerals.GetValue(Input);
+                    Operation = String.Join(" ", Output, "-", Input, "=");
+                    Output = RomanNumerals.GetSymbol(value);
+                    Input = "N";
+                    break;
+                case "/":
+                    value = RomanNumerals.GetValue(Output) / RomanNumerals.GetValue(Input);
+                    Operation = String.Join(" ", Output, "/", Input, "=");
+                    Output = RomanNumerals.GetSymbol(value);
+                    Input = "N";
+                    break;
+                case "x":
+                    value = RomanNumerals.GetValue(Output) * RomanNumerals.GetValue(Input);
+                    Operation = String.Join(" ", Output, "*", Input, "=");
+                    Output = RomanNumerals.GetSymbol(value);
+                    Input = "N";
+                    break;
+                case "N":
+                    if (Input.Equals("N")) Output = "N";
+                    else Input = "N";
+                    break;
+                default:
+                    value = RomanNumerals.GetValue(Input) + RomanNumerals.Numerals[button[0]];
+                    Input = RomanNumerals.GetSymbol(value);
+                    break;
+            }
+        }
     }
 
     public class Notify : INotifyPropertyChanged {
@@ -41,7 +100,47 @@ namespace RomanCalculatorApp {
         }
     }
 
-    public class RomanCalculator {
+    public class RomanNumerals {
+        static public Dictionary<char, int> Numerals = new Dictionary<char, int>() {
+            {'M', 1000 },
+            {'D', 500 },
+            {'C', 100 },
+            {'L', 50 },
+            {'X', 10 },
+            {'V', 5 },
+            {'I', 1 },
+            {'N', 0 },
+        };
 
+        static public string GetSymbol(int value) {
+            if (value == 0) return "N";
+
+            IEnumerable<char> keys = Numerals.Keys;
+            StringBuilder sb = new StringBuilder();
+
+            for(int i = 0; i < keys.Count() - 1; i++) {
+                int amount = value / Numerals[keys.ElementAt(i)];
+                value %= Numerals[keys.ElementAt(i)];
+                for (int a = 0; a < amount; a++) sb.Append(keys.ElementAt(i));
+            }
+
+            return sb.ToString();
+        }
+
+        static public int GetValue(string symbols) {
+            int value = 0;
+            Dictionary<char, int> symbolsDict = new Dictionary<char, int>();
+
+            foreach(char symbol in symbols) {
+                if (symbolsDict.ContainsKey(symbol)) symbolsDict[symbol]++;
+                else symbolsDict.Add(symbol, 1);
+            }
+
+            foreach(char symbol in symbolsDict.Keys) {
+                value += Numerals[symbol] * symbolsDict[symbol];
+            }
+
+            return value;
+        }
     }
 }
