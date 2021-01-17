23fa652a1473a228f22dc23919a00533faf7c92a
Synergie van der Pluijm <synergievdp@gmail.com>
2021-01-17 18:20:25 +0100

    Calculate cabfare in a single day

---
+++ CabFareApp/CabFareApp/CabFare.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabFareApp {
+    public class CabFare {
+        public int BaseFare { get; private set; } = 100;
+        public int DayFare { get; private set; } = 25;
+        public int NightFare { get; private set; } = 45;
+        public double WeekendTax { get; private set; } = 0.15;
+        public TimeSpan StartWork { get; private set; } = new TimeSpan(8, 0, 0);
+        public TimeSpan EndWork { get; private set; } = new TimeSpan(18, 0, 0);
+
+        public int GetFare(int kilometers, DateTime startTime, DateTime endTime) {
+            int fare = 0;
+
+            fare += kilometers * BaseFare;
+
+            TimeSpan workhours = GetWorkHours(startTime, endTime);
+            fare += (int)workhours.TotalMinutes * DayFare;
+            fare += (int)((endTime - startTime) - workhours).TotalMinutes * NightFare;
+
+            if (IsWeekend(startTime)) fare = (int)(fare * (1 + WeekendTax));
+
+            return fare;
+        }
+
+        public TimeSpan GetWorkHours(DateTime start, DateTime end) {
+            TimeSpan startTime = start.TimeOfDay;
+            TimeSpan endTime = end.TimeOfDay;
+
+            if (endTime > StartWork && startTime < EndWork) {
+                if (startTime < StartWork) startTime = StartWork;
+                if (endTime > EndWork) endTime = EndWork;
+
+                return endTime - startTime;
+            } else return TimeSpan.Zero;
+        }
+
+        public bool IsWeekend(DateTime startTime) {
+            if(startTime.DayOfWeek == DayOfWeek.Saturday || startTime.DayOfWeek == DayOfWeek.Sunday ||
+                (startTime.DayOfWeek == DayOfWeek.Friday && startTime.TimeOfDay >= new TimeSpan(22,0,0)) ||
+                (startTime.DayOfWeek == DayOfWeek.Monday && startTime.TimeOfDay < new TimeSpan(7,0,0))) {
+                return true;
+            } else return false;
+        }
+    }
+}
---
+++ CabFareApp/CabFareAppTests/CabFareTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CabFareApp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabFareApp.Tests {
+    [TestClass()]
+    public class CabFareTests {
+        [TestMethod()]
+        public void GetFareTest() {
+            CabFare cabFare = new CabFare();
+
+            Assert.AreEqual((50 * 100) + (25 * 10 * 60), cabFare.GetFare(50, new DateTime(2021, 1, 5, 8, 0, 0), new DateTime(2021, 1, 5, 18, 0, 0)));
+            Assert.AreEqual((50 * 100) + (45 * 8 * 60), cabFare.GetFare(50, new DateTime(2021, 1, 5, 0, 0, 0), new DateTime(2021, 1, 5, 8, 0, 0)));
+            Assert.AreEqual((50 * 100) + (45 * 6 * 60), cabFare.GetFare(50, new DateTime(2021, 1, 5, 18, 0, 0), new DateTime(2021, 1, 6, 0, 0, 0)));
+            Assert.AreEqual((int)(((50 * 100) + (25 * 10 * 60))*1.15), cabFare.GetFare(50, new DateTime(2021, 1, 2, 8, 0, 0), new DateTime(2021, 1, 2, 18, 0, 0)));
+            Assert.AreEqual((int)(((50 * 100) + (45 * 8 * 60))*1.15), cabFare.GetFare(50, new DateTime(2021, 1, 2, 0, 0, 0), new DateTime(2021, 1, 2, 8, 0, 0)));
+            Assert.AreEqual((int)(((50 * 100) + (45 * 6 * 60))*1.15), cabFare.GetFare(50, new DateTime(2021, 1, 2, 18, 0, 0), new DateTime(2021, 1, 3, 0, 0, 0)));
+        }
+
+        [TestMethod()]
+        public void GetWorkHoursTest() {
+            CabFare cabFare = new CabFare();
+
+            Assert.AreEqual(new TimeSpan(10,0,0), cabFare.GetWorkHours(new DateTime(2021, 1, 1, 8, 0, 0), new DateTime(2021, 1, 1, 18, 0, 0)));
+            Assert.AreEqual(TimeSpan.Zero, cabFare.GetWorkHours(new DateTime(2021, 1, 1, 0, 0, 0), new DateTime(2021, 1, 1, 8, 0, 0)));
+            Assert.AreEqual(TimeSpan.Zero, cabFare.GetWorkHours(new DateTime(2021, 1, 1, 18, 0, 0), new DateTime(2021, 1, 2, 0, 0, 0)));
+            Assert.AreEqual(new TimeSpan(1, 0, 0), cabFare.GetWorkHours(new DateTime(2021, 1, 1, 8, 0, 0), new DateTime(2021, 1, 1, 9, 0, 0)));
+            Assert.AreEqual(new TimeSpan(1, 0, 0), cabFare.GetWorkHours(new DateTime(2021, 1, 1, 7, 0, 0), new DateTime(2021, 1, 1, 9, 0, 0)));
+            Assert.AreEqual(new TimeSpan(1, 0, 0), cabFare.GetWorkHours(new DateTime(2021, 1, 1, 17, 0, 0), new DateTime(2021, 1, 1, 19, 0, 0)));
+            Assert.AreEqual(new TimeSpan(10, 0, 0), cabFare.GetWorkHours(new DateTime(2021, 1, 1, 7, 0, 0), new DateTime(2021, 1, 1, 19, 0, 0)));
+        }
+
+        [TestMethod()]
+        public void IsWeekendTest() {
+            CabFare cabFare = new CabFare();
+
+            Assert.AreEqual(false, cabFare.IsWeekend(new DateTime(2021, 1, 1, 0, 0, 0)));
+            Assert.AreEqual(true, cabFare.IsWeekend(new DateTime(2021, 1, 1, 22, 0, 0)));
+            Assert.AreEqual(true, cabFare.IsWeekend(new DateTime(2021, 1, 2, 0, 0, 0)));
+            Assert.AreEqual(true, cabFare.IsWeekend(new DateTime(2021, 1, 3, 0, 0, 0)));
+            Assert.AreEqual(true, cabFare.IsWeekend(new DateTime(2021, 1, 4, 0, 0, 0)));
+            Assert.AreEqual(false, cabFare.IsWeekend(new DateTime(2021, 1, 4, 7, 0, 0)));
+        }
+    }
+}
