eb83d60a77dee6c93d30f55bee92555c29dd06a8
Synergie van der Pluijm <synergievdp@gmail.com>
2021-01-18 16:26:36 +0100

    Create project.

---
+++ ParentalContributionApp/ParentalContributionApp/README.md
@@ -0,0 +1,9 @@
+﻿- [ ] Model  
+    - [ ] Every child younger than 10, add €25 (max 3 children)   
+    - [ ] Every children 10 and older, add €37 (max 2 children)   
+    - [ ] Max contribution €150   
+    - [ ] Single parent contribution 25% reduction  
+
+- [ ] View
+
+- [ ] ViewModel       
---
+++ ParentalContributionApp/ParentalContributionApp/RelayCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ParentalContributionApp {
+    class RelayCommand : NotifyPropertyChanged, ICommand {
+        readonly Action<object> _execute;
+        readonly Predicate<object> _canExecute;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute) {
+            _execute = execute ?? throw new NullReferenceException("execute");
+            _canExecute = canExecute;
+        }
+
+        public RelayCommand(Action<object> execute) : this(execute, null) { }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter) {
+            _execute.Invoke(parameter);
+        }
+
+        public void RaiseCanExecuteChanged() {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
